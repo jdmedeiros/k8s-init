61bfabcb93cdd44a1cba17266d4034409ce2c875
Dan Medeiros <dan.medeiros@sagewell.com>
2023-11-09 17:55:28 -0100

    POC Kubernetes init container

--- toUpper/app.py
+++ toUpper/app.py
@@ -1,1 +1,41 @@
-print("Hello World")
+import csv
+import os
+import logging
+
+# Configure the logger
+log_dir = '/data/logs'
+log_filename = 'app.log'
+log_path = os.path.join(log_dir, log_filename)
+
+logging.basicConfig(
+    filename=log_path,
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+incoming_dir = '/data/incoming'
+processed_dir = '/data/processed'
+
+for filename in os.listdir(incoming_dir):
+    if filename.endswith('.csv'):
+        input_file = os.path.join(incoming_dir, filename)
+        output_file = os.path.join(processed_dir, filename)
+
+        try:
+            with open(input_file, 'r') as csv_file:
+                reader = csv.reader(csv_file)
+
+                with open(output_file, 'w') as processed_csv:
+                    writer = csv.writer(processed_csv)
+                    for row in reader:
+                        processed_row = [cell.upper() for cell in row]
+                        writer.writerow(processed_row)
+
+            logging.info(f"Processed file: {input_file}")
+        except FileNotFoundError:
+            logging.error(f"CSV file not found at: {input_file}")
+        except Exception as e:
+            logging.error(f"An error occurred: {str(e)}")
+
+# Close the log file handler
+logging.shutdown()
